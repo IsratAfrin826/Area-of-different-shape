66badaa13c898de0de84c80f78a6cd28e61a7fb4
Israt Arefin <nisratafrin826@gmail.com>
2025-05-06 15:17:06 +0600

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+
+    int length ,area;
+
+    cout<< "Enter length : ";
+    cin>>length;
+
+    area= length * length ;
+
+    cout<<"Area of length  = "<<area;
+
+
+getch();
+}
